8aa652426feb5767080270845a97536777c0534c
George Connor <grgcnnr@gmail.com>
2014-08-03 15:57:34 +1200

    Wrapping in AMD export wrapper

--- dragondrop.js
+++ dragondrop.js
@@ -1,48 +1,54 @@
 /*
- * Dropit v1.1.0
- * http://dev7studios.com/dropit
+ * Dragondrop v0.0.0
+ * Based on http://dev7studios.com/dropit
  *
- * Copyright 2012, Dev7studios
  * Free to use and abuse under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
  */
 
-;(function($) {
-
-    $.fn.dropit = function(method) {
+;(function (f) {
+if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module depending on jQuery.
+    define(['jquery'], f);
+} else {
+    // No AMD. Register plugin with global jQuery object.
+    f(jQuery);
+}
+}(function ($) {
+    $.fn.dragonDrop = function(method) {
 
         var methods = {
 
             init : function(options) {
-                this.dropit.settings = $.extend({}, this.dropit.defaults, options);
+                this.dragonDrop.settings = $.extend({}, this.dragonDrop.defaults, options);
                 return this.each(function() {
                     var $el = $(this),
                          el = this,
-                         settings = $.fn.dropit.settings;
+                         settings = $.fn.dragonDrop.settings;
 
                     // Hide initial submenus
-                    $el.addClass('dropit')
-                    .find('>'+ settings.triggerParentEl +':has('+ settings.submenuEl +')').addClass('dropit-trigger')
-                    .find(settings.submenuEl).addClass('dropit-submenu').hide();
+                    $el.addClass('dragonDrop')
+                    .find('>'+ settings.triggerParentEl +':has('+ settings.submenuEl +')').addClass('dragonDrop-trigger')
+                    .find(settings.submenuEl).addClass('dragonDrop-submenu').hide();
 
                     // Open on click
                     $el.on(settings.action, settings.triggerParentEl +':has('+ settings.submenuEl +') > '+ settings.triggerEl +'', function(){
                         // Close click menu's if clicked again
-                        if(settings.action == 'click' && $(this).parents(settings.triggerParentEl).hasClass('dropit-open')){
+                        if(settings.action == 'click' && $(this).parents(settings.triggerParentEl).hasClass('dragonDrop-open')){
                             settings.beforeHide.call(this);
-                            $(this).parents(settings.triggerParentEl).removeClass('dropit-open').find(settings.submenuEl).hide();
+                            $(this).parents(settings.triggerParentEl).removeClass('dragonDrop-open').find(settings.submenuEl).hide();
                             settings.afterHide.call(this);
                             return false;
                         }
 
                         // Hide open menus
                         settings.beforeHide.call(this);
-                        $('.dropit-open').removeClass('dropit-open').find('.dropit-submenu').hide();
+                        $('.dragonDrop-open').removeClass('dragonDrop-open').find('.dragonDrop-submenu').hide();
                         settings.afterHide.call(this);
 
                         // Open this menu
                         settings.beforeShow.call(this);
-                        $(this).parents(settings.triggerParentEl).addClass('dropit-open').find(settings.submenuEl).show();
+                        $(this).parents(settings.triggerParentEl).addClass('dragonDrop-open').find(settings.submenuEl).show();
                         settings.afterShow.call(this);
 
                         return false;
@@ -51,7 +57,7 @@
                     // Close if outside click
                     $(document).on('click', function(){
                         settings.beforeHide.call(this);
-                        $('.dropit-open').removeClass('dropit-open').find('.dropit-submenu').hide();
+                        $('.dragonDrop-open').removeClass('dragonDrop-open').find('.dragonDrop-submenu').hide();
                         settings.afterHide.call(this);
                     });
 
@@ -59,7 +65,7 @@
                     if(settings.action == 'mouseenter'){
                         $el.on('mouseleave', function(){
                             settings.beforeHide.call(this);
-                            $(this).removeClass('dropit-open').find(settings.submenuEl).hide();
+                            $(this).removeClass('dragonDrop-open').find(settings.submenuEl).hide();
                             settings.afterHide.call(this);
                         });
                     }
@@ -75,12 +81,12 @@
         } else if (typeof method === 'object' || !method) {
             return methods.init.apply(this, arguments);
         } else {
-            $.error( 'Method "' +  method + '" does not exist in dropit plugin!');
+            $.error( 'Method "' +  method + '" does not exist in dragonDrop plugin!');
         }
 
     };
 
-    $.fn.dropit.defaults = {
+    $.fn.dragonDrop.defaults = {
         action: 'click', // The open action for the trigger
         submenuEl: 'ul', // The submenu element
         triggerEl: 'a', // The trigger element
@@ -92,6 +98,6 @@
         afterHide: function(){} // Triggers before submenu is hidden
     };
 
-    $.fn.dropit.settings = {};
+    $.fn.dragonDrop.settings = {};
 
-})(jQuery);
+}));
