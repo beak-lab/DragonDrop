6eef7c0a9a487fd1b18a6df8db903686b3d737e4
George Connor <grgcnnr@gmail.com>
2014-08-04 19:30:47 +1200

    Added scss and made the thing a bit more efficent

--- dragondrop.js
+++ dragondrop.js
@@ -6,103 +6,107 @@
  * http://www.opensource.org/licenses/mit-license.php
  */
 
-;(function (f) {
-    /* global jQuery, define */
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module depending on jQuery.
-        define(['jquery'], f);
-    } else {
-        // No AMD. Register plugin with global jQuery object.
-        f(jQuery);
-    }
+
+(function (f) {
+	/* global jQuery, define */
+	'use strict';
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module depending on jQuery.
+		define(['jquery'], f);
+	} else {
+		// No AMD. Register plugin with global jQuery object.
+		f(jQuery);
+	}
 }
 
 (function ($) {
-    'use strict';
+	'use strict';
 
-    $.fn.dragonDrop = function(method) {
-        var methods = {
+	$.fn.dragonDrop = function(method) {
+		var methods = {
 
-            init : function(options) {
-                this.dragonDrop.settings = $.extend({}, this.dragonDrop.defaults, options);
-                return this.each(function() {
-                        var $el = $(this).addClass('dragonDrop'),
-                        settings = $.fn.dragonDrop.settings,
-                        $menuElement = $el.find(settings.submenuEl).addClass('dragonDrop-submenu').hide(),
-                        $triggerElement = $el.find(settings.triggerEl).addClass('dragonDrop-trigger');
+			init : function(options) {	
+				return this.each(function() {
+						var $el = $(this).addClass('dragonDrop'),
+						settings = $.extend({}, $.fn.dragonDrop.defaults, options),
+						$menuElement = $('#' + $el.data('dropdown') );
+						$menuElement.addClass('dragonDrop-submenu');
 
-                    // Open on click
-                    $el.on(settings.action,  $triggerElement, function(){
-                        // Close click menus if clicked again
-                        if(settings.action == 'click' && $el.hasClass('dragonDrop-open')){
-                            settings.beforeHide.call(this);
-                            $el.removeClass('dragonDrop-open');
-                            $menuElement.hide();
-                            settings.afterHide.call(this);
-                            return false;
-                        }
+					// Open on click
+					$el.on(settings.action,  $el, function(){
+						// Close click menus if clicked again
+						if(settings.action == 'click' && $el.hasClass(settings.openClass)){
+							settings.beforeHide.call(this);
+							$el.removeClass(settings.openClass);
+							$menuElement.removeClass(settings.openClass);
+							settings.afterHide.call(this);
+							return false;
+						}
 
-                        // Hide ALL open menus
-                        settings.beforeHide.call(this);
-                        $('.dragonDrop-open').removeClass('dragonDrop-open').find('.dragonDrop-submenu').hide();
-                        settings.afterHide.call(this);
+						// Hide ALL open menus
+						settings.beforeHide.call(this);
+						$('.dragonDrop').removeClass(settings.openClass);
+						$('.dragonDrop-submenu').removeClass(settings.openClass);
+						settings.afterHide.call(this);
 
-                        // Open this menu
-                        settings.beforeShow.call(this);
-                        $el.addClass('dragonDrop-open');
-                        $menuElement.show();
+						// Open this menu
+						settings.beforeShow.call(this);
+						$el.addClass(settings.openClass);
+						$menuElement.addClass(settings.openClass);
 
-                        settings.afterShow.call(this);
+						settings.afterShow.call(this);
 
-                        return false;
-                    });
+						return false;
+					});
 
-                    // Close if outside click
-                    $(document).on('click', function(){
-                        settings.beforeHide.call(this);
-                        $('.dragonDrop-open').removeClass('dragonDrop-open');
-                        $menuElement.hide();
-                        settings.afterHide.call(this);
-                    });
+					// Close if outside click
+					$(document).on('click', function(){
+						settings.beforeHide.call(this);
+						$('.dragonDrop').removeClass(settings.openClass);
+						$menuElement.removeClass(settings.openClass);
+						settings.afterHide.call(this);
+					});
 
-                    // If hover
-                    if(settings.action == 'mouseenter'){
-                        $el.on('mouseleave', function(){
-                            settings.beforeHide.call(this);
-                            $el.removeClass('dragonDrop-open');
-                            $menuElement.hide();
-                            settings.afterHide.call(this);
-                        });
-                    }
+					// If hover
+					if(settings.action == 'mouseenter'){
+						$el.on('mouseleave', function(){
+							settings.beforeHide.call(this);
+							$('.dragonDrop').removeClass(settings.openClass);
+							$menuElement.removeClass(settings.openClass);
+							settings.afterHide.call(this);
+						});
+					}
 
-                    settings.afterLoad.call(this);
-                });
-            }
+					settings.afterLoad.call(this);
+				});
+			},
 
-        };
+			// register: function(){
+			// 	$(window).trigger('dragonDropRegister' [this.$el]);
+			// }
 
-        if (methods[method]) {
-            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-        } else if (typeof method === 'object' || !method) {
-            return methods.init.apply(this, arguments);
-        } else {
-            $.error( 'Method "' +  method + '" does not exist in dragonDrop plugin!');
-        }
+		};
 
-    };
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error( 'Method "' +  method + '" does not exist in dragonDrop plugin!');
+		}
 
-    $.fn.dragonDrop.defaults = {
-        action: 'click', // The open action for the trigger
-        submenuEl: '[data-menu-content]', // The submenu element
-        triggerEl: '[data-menu-trigger]', // The trigger element
-        afterLoad: function(){}, // Triggers when plugin has loaded
-        beforeShow: function(){}, // Triggers before submenu is shown
-        afterShow: function(){}, // Triggers after submenu is shown
-        beforeHide: function(){}, // Triggers before submenu is hidden
-        afterHide: function(){} // Triggers before submenu is hidden
-    };
+	};
 
-    $.fn.dragonDrop.settings = {};
+	$.fn.dragonDrop.defaults = {
+		action		: 'click', // The open action for the trigger
+		triggerEl	: '[data-dropdown]', // The trigger element
+		openClass	: 'is-open',
+		afterLoad	: function(){}, // Triggers when plugin has loaded
+		beforeShow	: function(){}, // Triggers before submenu is shown
+		afterShow	: function(){}, // Triggers after submenu is shown
+		beforeHide	: function(){}, // Triggers before submenu is hidden
+		afterHide	: function(){} // Triggers before submenu is hidden
+	};
+
 
 }));
